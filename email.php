db443519354fc9b07bb1143668f9f9a1ccc2f545
gilsonbezerra <gilson.bezerra@live.com>
2019-10-24 20:38:40 -0300

    Update

--- email.php
+++ email.php
@@ -1,36 +1,50 @@
 <?php
-
-if (isset($_POST['email']) {
-
-    $nome = addcslashes($_POST['nome']);
-    $email = addcslashes($_POST['email']);
-    $cidade = addcslashes($_POST['cidade']);
-    $estado = addcslashes($_POST['estado']);
-    $cep = addcslashes($_POST['cep']);
-    $mensagem = addcslashes($_POST(['mensagem']));
-
-    $to = "contato@lithiumcode.com.br";
-    $subject = "Contato do Site";
-    $body = "Nome: ".$nome. "\n".
-            "Email: ".$email. "\n".
-            "Mensagem: ".$mensagem;
-
-    $header = "From: contato@lithiumcode.com.br"."\n"
-                ."Reply-To:".$email. "\n"
-                ."X=Mailer:PHP/".phpversion();
-
-    if(mail($to, $subject, $body, $header)){
-        echo("Email enviado com sucesso!");
-    }else{
-        echo("O email não pode ser enviado");
-    }
-
-    
-}
-
-
-
-
-
-
+if (isset($_POST['BTEnvia'])) {
+ 
+ //Variaveis de POST, Alterar somente se necessário 
+ //====================================================
+ $nome = $_POST['nome'];
+ $email = $_POST['email'];
+ $cidade = $_POST['cidade'];
+ $estado = $_POST['estado'];
+ $cep = $_POST['cep'];
+ $mensagem = $_POST['mensagem'];
+ //====================================================
+ 
+ //REMETENTE --> ESTE EMAIL TEM QUE SER VALIDO DO DOMINIO
+ //==================================================== 
+ $email_remetente = "contato@lithiumcode.com.br"; // deve ser uma conta de email do seu dominio 
+ //====================================================
+ 
+ //Configurações do email, ajustar conforme necessidade
+ //==================================================== 
+ $email_destinatario = "contato@lithiumcode.com.br"; // pode ser qualquer email que receberá as mensagens
+ $email_reply = "$email"; 
+ $email_assunto = "Contato do Site"; // Este será o assunto da mensagem
+ //====================================================
+ 
+ //Monta o Corpo da Mensagem
+ //====================================================
+ $email_conteudo = "Nome = $nome \n"; 
+ $email_conteudo .= "Email = $email \n";
+ $email_conteudo .= "Cidade = $cidade \n"; 
+ $email_conteudo .= "Estado = $estado \n"; 
+ $email_conteudo .= "Cep = $cep \n"; 
+ $email_conteudo .= "Mensagem = $mensagem \n"; 
+ //====================================================
+ 
+ //Seta os Headers (Alterar somente caso necessario) 
+ //==================================================== 
+ $email_headers = implode ( "\n",array ( "From: $email_remetente", "Reply-To: $email_reply", "Return-Path: $email_remetente","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ) );
+ //====================================================
+ 
+ //Enviando o email 
+ //==================================================== 
+ if (mail ($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers)){ 
+ echo "</b>E-Mail enviado com sucesso!</b>"; 
+ } 
+ else{ 
+ echo "</b>Falha no envio do E-Mail!</b>"; } 
+ //====================================================
+} 
 ?>
