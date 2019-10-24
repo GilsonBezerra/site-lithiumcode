288528809afc50da2275d8e89dfaf23ff4329ff4
gilsonbezerra <gilson.bezerra@live.com>
2019-10-24 20:23:03 -0300

    Update

--- email.php
+++ email.php
@@ -1,23 +1,23 @@
 <?php
 
-if (isset($_POST(['email']) && !=empty($_POST(['email'])) {
+if (isset($_POST['email'] && !=empty($_POST['email']) {
 
-    $nome = addcslashes($_POST(['nome']))
-    $email = addcslashes($_POST(['email']))
-    $cidade = addcslashes($_POST(['cidade']))
-    $estado = addcslashes($_POST(['estado']))
-    $cep = addcslashes($_POST(['cep']))
+    $nome = addcslashes($_POST['nome'])
+    $email = addcslashes($_POST['email'])
+    $cidade = addcslashes($_POST['cidade'])
+    $estado = addcslashes($_POST['estado'])
+    $cep = addcslashes($_POST['cep'])
     $mensagem = addcslashes($_POST(['mensagem']))
 
     $to = "contato@lithiumcode.com.br";
     $subject = "Contato do Site";
-    $body = "Nome: ".$nome. "\n"
-            "Email: ".$email. "\n"
+    $body = "Nome: ".$nome. "\n".
+            "Email: ".$email. "\n".
             "Mensagem: ".$mensagem;
 
     $header = "From: contato@lithiumcode.com.br"."\n"
                 ."Reply-To:".$email. "\n"
-                "X=Mailer:PHP/".phpversion();
+                ."X=Mailer:PHP/".phpversion();
 
     if(mail($to, $subject, $body, $header)){
         echo("Email enviado com sucesso!");
