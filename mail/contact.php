48351f09aaa1a02865fbd12029781a4a34f27738
Gilson Bezerra <gilson.bezerra@live.com>
2024-05-13 18:09:01 -0300

    teste

--- mail/contact.php
+++ mail/contact.php
@@ -6,7 +6,7 @@ if(empty($name = $_POST['name'])           ||
    empty($message = $_POST['message'])     ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
       echo  "<script>
-               window.location='index.html';
+               window.location='contact.html';
                alert('Todos os campos são de preenchimento obrigatório!');
             </script>"; ;
       return "index.html";
@@ -18,7 +18,7 @@ $email_subject = "Contato do site de:  $name";
 $email_body = "Você acaba de receber um novo contato via site Lithium Code!\n\n"."Veja os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensagem:\n\n$message";
 // $headers = "From: contato@lithiumcode.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 // $headers .= "Reply-To: $email_address"; 
-$email_headers = implode ("\n",array ( "From: contato@lithiumproducoes.com.br", "Reply-To: $email_address", "Return-Path: $email_address","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ));  
+$email_headers = implode ("\n",array ( "From: contato@lithiumcode.com.br", "Reply-To: $email_address", "Return-Path: $email_address","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ));  
 mail($to,$email_subject,nl2br($email_body),$email_headers);
 return true;         
 ?>
