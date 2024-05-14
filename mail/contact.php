9a56b7eb41be2dd2c8e0bf5f31c746b673b4b207
Gilson Bezerra <gilson.bezerra@live.com>
2024-05-14 14:11:13 -0300

    teste

--- mail/contact.php
+++ mail/contact.php
@@ -4,7 +4,7 @@ if (isset($_POST['btnEnviar'])) {
 
     $msg = '';
     
-    //campos do formulario
+
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $assunto = $_POST["subject"];
@@ -13,10 +13,10 @@ if (isset($_POST['btnEnviar'])) {
     //valida se os campos não estão vazios
     if ((!empty($nome)) && (!empty($email)) && (!empty($assunto)) && (!empty($mensagem))) {
         
-        $email_remetente = "formulario_site@lithiumcode.com.br"; // deve ser uma conta de email do seu dominio 
-        $email_destinatario = $email; // email que receberá as mensagens
+        $email_remetente = "formulario_site@lithiumcode.com.br";
+        $email_destinatario ="contato@lithiumcode.com.br";
         $email_recebidoDe = "$email";
-        $email_assunto = $assunto; // Este será o assunto da mensagem
+        $email_assunto = $assunto;
         $email_conteudo = "FORMULÁRIO DE CONTATO\n"
             . "<br><b>De:</b> " . $nome
             . "<br><b>Email:</b> " . $email
@@ -26,21 +26,19 @@ if (isset($_POST['btnEnviar'])) {
             . "<hr>"
             . "<br>Mensagem enviada do formulário de contato da demonstração de formulário de contato com php.";
 
-        //encapsula os dados do cabeçalho do email
+        
         $email_cabecalho = implode("\n", array("From: $email_remetente", "Reply-To: $email_recebidoDe", "Return-Path: $email_remetente", "MIME-Version: 1.0", "X-Priority: 3", "Content-Type: text/html; charset=UTF-8"));
         
-        //utiliza função nativa do php mail para o envio
-        //valida se o email foi enviado
+        
         if (mail($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_cabecalho)) {
             
-            //mostra mensagem de envio com sucesso
+           
             $msg = '<div class="alert alert-success alert-dismissable">
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                 <strong>Mensagem enviada com sucesso!</strong> 
             </div>';
         } else {
 
-            //mostra mensagem de erro ao enviar
             $msg = '<div class="alert alert-danger alert-dismissable">
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                 <strong>Erro ao enviar mensagem, tente novamente! </strong> 
@@ -48,8 +46,7 @@ if (isset($_POST['btnEnviar'])) {
         }
         
     } else {
-        
-        //mostra mensagem de erro caso algum dos campos esteja vazio
+
         $msg = '<div class="alert alert-danger alert-dismissable">
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                 <strong>Preencha todos os campos!! </strong> 
